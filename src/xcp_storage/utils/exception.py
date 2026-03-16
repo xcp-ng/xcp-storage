e313c7aadac79ca26f854bfce808291a385711cb
Ronan Abhamon <ronan.abhamon@vates.tech>
2026-03-16 23:53:30 +0100

    feat(core): add JSON-RPC helpers
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.tech>

---
+++ src/xcp_storage/utils/exception.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2026  Vates SAS
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# ==============================================================================
+
+def stringify_exception(e: Exception) -> str:
+    try:
+        return str(e)
+    except Exception:
+        try:
+            return f"Exception of type `{e.__class__.__name__}`."
+        except Exception:
+            return "Unknown Exception."
